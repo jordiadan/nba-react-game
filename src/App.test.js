3cdec52fd17edc22682f6f8c582b9e6c34ee2d04
Jordi Adan <jordi.adan@adevinta.com>
2024-02-03 18:45:30 +0100

    Add App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,11 @@
 
+// App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders App component', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/NBA Card Gacha Game/i);
+  expect(linkElement).toBeInTheDocument();
+});
