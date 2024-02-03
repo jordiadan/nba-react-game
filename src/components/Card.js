a6c89d488ffae2d32d67504a3c27a326754f22d1
Jordi Adan <jordi.adan@adevinta.com>
2024-02-03 19:40:00 +0100

    Add card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,41 +1,18 @@
-// Card.js
 import React from 'react';
-
-const cardStyles = {
-  border: '1px solid #ddd',
-  borderRadius: '8px',
-  width: '200px',
-  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-  textAlign: 'center',
-  padding: '8px',
-  backgroundColor: '#f8f8f8',
-};
-
-const imageStyles = {
-  width: '100%',
-  borderRadius: '4px',
-  marginBottom: '8px',
-};
-
-const titleStyles = {
-  fontSize: '16px',
-  fontWeight: 'bold',
-  marginBottom: '4px',
-};
-
-const statsStyles = {
-  fontSize: '14px',
-  color: '#555',
-};
+import './Card.css';
 
 const Card = ({ player }) => {
+  const rarityClass = player.rarity.toLowerCase(); // Assuming rarity is in lowercase
+
   return (
-    <div style={cardStyles}>
-      <img src={player.image} alt={`${player.playerName} Card`} style={imageStyles} />
+    <div className={`card ${rarityClass}`}>
+      <div className="inner-border">
+        <div className="avg-power-overlay">{player.avgPower}</div>
+        <img src="sample_image.jpg" alt={`${player.playerName} Card`} className="card-img" />
+      </div>
       <div>
-        <p style={titleStyles}>{player.playerName}</p>
-        <p style={statsStyles}>{`Avg Power: ${player.avgPower}`}</p>
-        <p style={statsStyles}>{`Team: ${player.team}`}</p>
+        <p className="card-title">{player.playerName}</p>
+        <p className="card-stats">{`Team: ${player.team}`}</p>
         {/* Add more card details as needed */}
       </div>
     </div>
