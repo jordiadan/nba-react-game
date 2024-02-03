1a9ae61792803c203588eb241dc5f51aa2ef4c10
Jordi Adan <jordi.adan@adevinta.com>
2024-02-03 18:54:49 +0100

    Create CardCollectionView

---
+++ src/components/CardCollectionView.js
@@ -0,0 +1,19 @@
+// CardCollectionView.js
+import React from 'react';
+
+const CardCollectionView = () => {
+  // Component implementation
+
+  return (
+    <div className="card-collection-container">
+      <header>
+        <h2>Card Collection</h2>
+      </header>
+      <main>
+        {/* Display cards, filters, sorting options, etc. */}
+      </main>
+    </div>
+  );
+};
+
+export default CardCollectionView;
